65120b6e21d463b93871c1028afcdf9d0eb5b146
Mona Fakhri <mona.fakhri1374@gmail.com>
2026-09-01 19:02:29 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from src.modis_download import download_modis
+from src.coverage_analysis import calculate_coverage
+from src.visualization import create_map
+
+
 def main():
-    print("MODIS Iran Coverage Pipeline")
+
+    granules = download_modis()
+
+    results = calculate_coverage(granules)
+
+    create_map(results)
 
 
 if __name__ == "__main__":
